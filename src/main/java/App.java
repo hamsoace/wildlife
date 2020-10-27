0591a7c44f8bd5a1da7c6689ffc1b02e8b280c57
hamsoace <hamsoace@gmail.com>
2020-10-27 18:56:53 +0300

    added database, endangered class and animal class

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+//    public static void main(String[] args) {
+//        Map<String, Object> model = new HashMap<>();
+//        String index = "index.hbs";
+//
+//        get("/", (request, response) -> {
+//            model.put("animals", Animal.all());
+//            return new ModelAndView(model, index);
+//        }, new HandlebarsTemplateEngine());
+//    }
+}
