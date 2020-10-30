9a37eabe38a5cdd1a2fdb8ceaaf0248055490dcb
hamsoace <hamsoace@gmail.com>
2020-10-30 11:21:45 +0300

    debug 500 internal server

--- db.sql
+++ db.sql
@@ -1,3 +1,5 @@
+SET MODE postgresql;
+
 CREATE DATABASE wildlife;
 
 \c wildlife
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,26 @@
 import models.Endangered;
+import models.Sightings;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static spark.Spark.*;
 
 public class App{
     public static void main(String[] args) {
-//        Endangered animal = new Endangered(1, "White Rhino");
-        System.out.println("Nafanya Kazi!");
+       // System.out.println("Nafanya Kazi!");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Sightings> allSightings = Sightings.all();
+            model.put("sightings", allSightings);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
     }
 }
