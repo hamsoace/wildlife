88466d44b8eeb6c8786628404c4802d32f881976
hamsoace <hamsoace@gmail.com>
2020-11-05 10:45:55 +0300

    animal rangerfinished

--- src/test/java/models/AnimalRangerTest.java
+++ src/test/java/models/AnimalRangerTest.java
@@ -14,4 +14,18 @@ public class AnimalRangerTest {
         AnimalRanger animalRanger = newAnimalRanger();
         assertTrue(animalRanger instanceof AnimalRanger);
     }
+
+    @Test
+    public void getName_returnAnimalRangerName_true(){
+        AnimalRanger animalRanger = newAnimalRanger();
+        assertEquals("Hamso", animalRanger.getName());
+    }
+
+    @Test
+    public void save_getAnimalRangerId(){
+        AnimalRanger animalRanger = newAnimalRanger();
+        int idBefore = animalRanger.getId();
+        animalRanger.save();
+        assertNotEquals(idBefore, animalRanger.getId());
+    }
 }
