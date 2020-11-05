6e19785686d73d644478b8ff69810ffb9a4ecdf8
hamsoace <hamsoace@gmail.com>
2020-11-05 11:08:52 +0300

    finished endangered tests

--- src/test/java/models/EndangeredTest.java
+++ src/test/java/models/EndangeredTest.java
@@ -20,4 +20,16 @@ public class EndangeredTest {
         Endangered endangered = newEndageredAnimal();
         assertEquals("BullFrog", endangered.getName());
     }
+
+    @Test
+    public void getHealth_returnAnimalsHealth(){
+        Endangered endangered = newEndageredAnimal();
+        assertEquals("Healthy", endangered.getHealth());
+    }
+
+    @Test
+    public void getAge_returnAnimalsAge(){
+        Endangered endangered = newEndageredAnimal();
+        assertEquals("Young", endangered.getAge());
+    }
 }
